c1fe58257259ef1e9c9829b40cbaa52638994d43
Vezel <Gustav@Gustavs-MacBook-Air.local>
2015-03-13 15:12:39 +0100

    Start
    This is the start of the project.

---
+++ script/main.js
@@ -0,0 +1,18 @@
+function init() {
+    game = document.getElementById('camera');
+    ctx = game.getContext('2d');
+    
+    window.setInterval(update, 25);
+}
+
+function keyHandler() {
+    
+}
+
+function keyDown() {
+    
+}
+
+function update() {
+    
+}
